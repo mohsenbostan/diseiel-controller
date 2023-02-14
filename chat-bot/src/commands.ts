e018618fb3172371c7258f53682c66eecea28b7e
Mohsen Bostan <mohsenbostandev@gmail.com>
2023-02-15 00:56:55 +0330

    refactor(chat-bot): increase mouse move

--- chat-bot/src/commands.ts
+++ chat-bot/src/commands.ts
@@ -3,7 +3,7 @@ import robot from "robotjs";
 const moveMouseCmd = async (): Promise<void> => {
   console.log("MOVE MOUSE");
   const { x, y } = robot.getMousePos();
-  robot.moveMouse(x + 10, y - 10);
+  robot.moveMouse(x + 30, y - 50);
 };
 
 const commands: Record<string, Function> = {
