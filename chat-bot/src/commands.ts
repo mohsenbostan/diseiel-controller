706b1e169584141371afd027ca00bcf279dec412
Mohsen Bostan <mohsenbostandev@gmail.com>
2023-02-15 01:00:50 +0330

    feat(chat-bot): add click event

--- chat-bot/src/commands.ts
+++ chat-bot/src/commands.ts
@@ -6,8 +6,14 @@ const moveMouseCmd = async (): Promise<void> => {
   robot.moveMouse(x + 30, y - 50);
 };
 
+const clickMouseCmd = async (): Promise<void> => {
+  console.log("CLICK MOUSE");
+  robot.mouseClick();
+};
+
 const commands: Record<string, Function> = {
   "+mouse": moveMouseCmd,
+  "+click": clickMouseCmd,
 };
 
 export default commands;
